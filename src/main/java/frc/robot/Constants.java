d87a87c77c997995c544029aaf85a0f5de3f641e
Nonochen0104 <xchen26@moundsparkacademy.org>
2026-01-18 13:17:28 -0600

    Changed the angle Gear ratio to the correct ratio for the module that we are currently using. Fixed the SwerveDriveKinematics to match the ModuleData

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -41,15 +41,16 @@ public static final double motorSpeedMultiplier = 0.5; // Used to scale down mot
 
     public static final double driveGearRatio = (8.14 / 1.0); // 6.75:1 L2 Mk4 Modules
     //L1 is 8.14:1, L2 is 6.75:1, L3 is 6.12:1, L4 is 5.14:1
-    public static final double angleGearRatio = (21.4 / 1.0); // 12.8:1 MK4 SDS Modules
+    public static final double angleGearRatio = (12.8 / 1.0); // 12.8:1 MK4 SDS Modules
     //SDS Mk4 is 12.8:1,  Mk4i is 21.4:1
 
     public static final SwerveDriveKinematics swerveKinematics =
     new SwerveDriveKinematics(
-        new Translation2d(halfTrackWidth, halfWheelBase), //translation 2d locates the swerve module in cords
-        new Translation2d(halfTrackWidth,-halfWheelBase),
-        new Translation2d(-halfTrackWidth,-halfWheelBase),
-        new Translation2d(-halfTrackWidth, halfWheelBase));
+        new Translation2d(-halfTrackWidth, -halfWheelBase), //Back Right
+        new Translation2d(halfTrackWidth,-halfWheelBase), // Front Right
+        new Translation2d(halfTrackWidth,halfWheelBase), // Front Left
+        new Translation2d(-halfTrackWidth, halfWheelBase)); // Back Left
+    //translation 2d locates the swerve module in cords
     //https://docs.wpilib.org/en/stable/docs/software/kinematics-and-odometry/swerve-drive-kinematics.html
     //SwerveDrive Kinematics converts between a ChassisSpeeds object and several SwerveModuleState objects, 
     //which contains velocities and angles for each swerve module of a swerve drive robot.
