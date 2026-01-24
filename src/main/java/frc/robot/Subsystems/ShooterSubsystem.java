b8f2ce7408f7941c8cf4368b3f85c6c7da6fd6ac
FireheadReal <jwrich28@moundsparkacademy.org>
2026-01-24 12:35:06 -0600

    Shooter Subsystem

--- src/main/java/frc/robot/Subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/Subsystems/ShooterSubsystem.java
@@ -3,13 +3,28 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.Subsystems;
-
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import java.util.function.DoubleSupplier;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
 
 public class ShooterSubsystem extends SubsystemBase {
-  /** Creates a new ShooterSubsystem. */
+
+  public final int ID = 0; //Placeholder ID
+  SparkMax shooterMotor = new SparkMax((int) ID, MotorType.kBrushless);
+
+
   public ShooterSubsystem() {}
 
+
+    public Command ShootingControlCommand(DoubleSupplier speed) {
+      return runOnce(
+          () -> {
+            shooterMotor.set(MathUtil.applyDeadband(speed.getAsDouble(), 0.1));
+          });
+    }
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
