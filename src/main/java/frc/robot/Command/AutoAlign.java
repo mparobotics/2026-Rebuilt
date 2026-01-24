0ffd5dffe40312c3e2e335770de14ded8fdcfe2d
jmmaloney3 <jmmaloney3@gmail.com>
2026-01-24 14:45:39 -0600

    test reject merge to main

--- src/main/java/frc/robot/Command/AutoAlign.java
+++ src/main/java/frc/robot/Command/AutoAlign.java
@@ -9,6 +9,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants.FieldConstants;
 import frc.robot.Subsystems.SwerveSubsystem;
 
+# make a change
 public class AutoAlign extends Command {
     private SwerveSubsystem m_SwerveSubsystem;
 
