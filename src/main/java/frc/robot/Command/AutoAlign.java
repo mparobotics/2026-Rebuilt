0270239907adae08ed5b713edf737bf98e2cf494
Nonochen0104 <xchen26@moundsparkacademy.org>
2026-01-26 18:51:39 -0600

    start to code auto align

--- src/main/java/frc/robot/Command/AutoAlign.java
+++ src/main/java/frc/robot/Command/AutoAlign.java
@@ -10,43 +10,37 @@ import frc.robot.Constants.FieldConstants;
 import frc.robot.Subsystems.SwerveSubsystem;
 
 public class AutoAlign extends Command {
+
+
     private SwerveSubsystem m_SwerveSubsystem;
-
-    private PIDController xController = new PIDController(0.8, 0, 0);
-    private PIDController yController = new PIDController(0.8, 0, 0);
-    private PIDController rotationController = new PIDController(0.005, 0, 0);
-    private boolean isLeft;
-
-    private ChassisSpeeds getAutoAlignSpeed (Pose2d CurrentPosition, Translation2d ReefCenter, double DistanceFromReef, double BranchDistancefromMiddle){
-        if (isLeft){
-            BranchDistancefromMiddle *= -1;
-        }
-        Translation2d OffSet = CurrentPosition.getTranslation().minus(ReefCenter);
-        double goalAngle = Math.round((OffSet.getAngle().getDegrees())/60) * 60;
-        Rotation2d goalRotation = Rotation2d.fromDegrees(goalAngle);
-        Translation2d scoringLocation = new Translation2d(DistanceFromReef, BranchDistancefromMiddle);
-        scoringLocation = scoringLocation.rotateBy(goalRotation);
-        scoringLocation = scoringLocation.plus(ReefCenter);
-
-        double xOutput = xController.calculate(CurrentPosition.getX(), scoringLocation.getX());
-        double yOutput = yController.calculate(CurrentPosition.getY(), scoringLocation.getY());
-        double rotationOutput = rotationController.calculate(CurrentPosition.getRotation().getDegrees(), goalAngle + 180);
-        return new ChassisSpeeds(xOutput, yOutput, rotationOutput);
-    } 
-
-    public void execute(){
-        ChassisSpeeds ssppeeeedd = getAutoAlignSpeed(m_SwerveSubsystem.getPose(), FieldConstants.flipForAlliance(FieldConstants.BLUE_REEF_CENTER), 
-        1.55, 0.2);
-        m_SwerveSubsystem.driveFromChassisSpeeds(ChassisSpeeds.fromFieldRelativeSpeeds(ssppeeeedd, m_SwerveSubsystem.getPose().getRotation()), true);
-    }
-
-    public AutoAlign(SwerveSubsystem drive, boolean Left){
-        m_SwerveSubsystem = drive;
-        isLeft = Left;
-        rotationController.enableContinuousInput (-180, 180);
+    public AutoAlign(SwerveSubsystem SwerveSubsystem){
+        this.m_SwerveSubsystem = SwerveSubsystem;
         addRequirements(m_SwerveSubsystem);
     }
 
+
+
+    @Override
+    public void initialize(){
+
+    }
+
+
+    @Override
+    public void execute(){
+        Pose2d FieldPosition = m_SwerveSubsystem.getPose();    //Get robot position on field, in variable
+        Translation2d HubLocation = new Translation2d(12, 13); //Hub location -> CHANGE
+        
+    }
+
+    @Override
+    public void end(boolean interrupted){
+
+    }
+
+    @Override
+    public boolean isFinished(){
+        return false;
+    }
+
 }
-
-
