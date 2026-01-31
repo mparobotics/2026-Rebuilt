0b3e9c1a14d645eddc6d589e458022ae98918d1a
Nonochen0104 <xchen26@moundsparkacademy.org>
2026-01-31 17:17:38 -0600

    rezeroed

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -111,10 +111,10 @@ public static final double motorSpeedMultiplier = 0.5; // Used to scale down mot
     ){}
 
     public static ModuleData[] moduleData = {
-      new ModuleData(11, 52, 19, 340.32, BACK_RIGHT), //Mod 0 Back right
-      new ModuleData(17, 53, 22, 51.59, FRONT_RIGHT), //Mod 1 Front right
-      new ModuleData(15, 16, 21, 130.16, FRONT_LEFT), //Mod 2 Front left
-      new ModuleData(13, 12, 20, 118.47, BACK_LEFT) //Mod 3 Back left
+      new ModuleData(11, 52, 19, 159.25, BACK_RIGHT), //Mod 0 Back right
+      new ModuleData(17, 53, 22, 230.89, FRONT_RIGHT), //Mod 1 Front right
+      new ModuleData(15, 16, 21, 311.67, FRONT_LEFT), //Mod 2 Front left
+      new ModuleData(13, 12, 20, 297.69, BACK_LEFT) //Mod 3 Back left
     };
     
   }
