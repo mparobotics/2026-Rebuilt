297dbc4d96abaeb0d4e8d8cafebb468617088cfe
Lilly <113945482+raininglilly@users.noreply.github.com>
2026-03-04 15:57:42 -0600

    e

--- src/main/java/frc/robot/Subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/Subsystems/ShooterSubsystem.java
@@ -144,7 +144,6 @@ public class ShooterSubsystem extends SubsystemBase {
     }
 
     public void AutoToggleShoot (boolean AutoShootOn) {
-      setKickerSpeed(AutoShootOn ? 0 : ShooterConstants.KICKER_SPEED);
       setShooterSpeed(AutoShootOn ? 0 : ShooterConstants.SHOOTER_SPEED);
     }
 
