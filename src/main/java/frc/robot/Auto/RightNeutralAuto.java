23e54f77657b4c0b70da1d64d8bef6c5050a1f53
Zachray <145231265+ZachrayL@users.noreply.github.com>
2026-03-13 15:04:29 -0500

    added right neutral zone auto

--- src/main/java/frc/robot/Auto/RightNeutralAuto.java
+++ src/main/java/frc/robot/Auto/RightNeutralAuto.java
@@ -16,12 +16,12 @@ import frc.robot.Subsystems.SwerveSubsystem;
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
-public class LeftNeutralAuto extends SequentialCommandGroup {
-  /** Creates a new LeftNeutralAuto. */
+public class RightNeutralAuto extends SequentialCommandGroup {
+  /** Creates a new RightNeutralAuto. */
   SwerveSubsystem m_drive;
   IntakeSubsystem m_intake;
   ShooterSubsystem m_shooter;
-  public LeftNeutralAuto(SwerveSubsystem drive, IntakeSubsystem intake, ShooterSubsystem shooter) {
+  public RightNeutralAuto(SwerveSubsystem drive, IntakeSubsystem intake, ShooterSubsystem shooter) {
     m_drive = drive;
     m_intake = intake;
     m_shooter = shooter; 
@@ -29,14 +29,13 @@ public class LeftNeutralAuto extends SequentialCommandGroup {
     // addCommands(new FooCommand(), new BarCommand());
     addCommands(
       // lower intake
-      new ParallelCommandGroup(
-        m_drive.startAutoAt(3.549, 7.253, 11.000),
-        Commands.runOnce(() -> m_intake.lowerIntake(), m_intake)
-      ),
+      m_drive.startAutoAt(3.549, 7.253, 11.000),
+
       
-      // drive to scoring position, raise hood, spin up shooter
+      // drive to scoring position, lower intake, raise hood, spin up shooter
       new ParallelCommandGroup(
-        Commands.run(() -> m_drive.autoDrive("left neutral start-shoot"), m_drive),
+        Commands.run(() -> m_drive.autoDrive("right neutral start-shoot"), m_drive),
+        Commands.runOnce(() -> m_intake.lowerIntake(), m_intake),
         Commands.runOnce(() -> m_shooter.setHoodAngle(ShooterSubsystem.HoodAngle.HIGH), m_shooter),
         Commands.run(() -> m_shooter.setShooterSpeed(ShooterConstants.SHOOTER_SPEED), m_shooter)
         .until(() -> m_shooter.getShooterVelocityRpm() >= ShooterConstants.SHOOTER_READY_RPM)
@@ -60,7 +59,7 @@ public class LeftNeutralAuto extends SequentialCommandGroup {
         ),
 
       // intake from middle
-      Commands.run(() -> m_drive.autoDrive("left neutral shoot-collect-shoot"), m_drive),
+      Commands.run(() -> m_drive.autoDrive("right neutral shoot-collect-shoot"), m_drive),
 
       // raise hood, then shoot
       Commands.runOnce(() -> m_shooter.setHoodAngle(ShooterSubsystem.HoodAngle.HIGH), m_shooter),
