97a429ba442b59f96811d72d15d20ed05caf409c
Gavin <gavin2noah@gmail.com>
2026-03-21 19:27:42 -0500

    Pre10k odometry update (#29)
    * fixed spelling of variable, added some safeguards for limelights, change back to megatag 2, lowered standard deviation for translation, updated updateOdometryWithVision method
    
    * Added tuningHelper class
    
    * changed tuning variables to only work in test mode
    
    * updated spelling mistake in gitignore for ctre_sim folder
    
    * added second intake arm motor (will probably need to be fixed)
    
    * added feedforward for intake arm
    
    * fixed feedforward (whoops but is okay now)
    
    * Add the id for the 2nd intake arm motor
    
    * Intake arm 2
    
    * intake arm 2 tuning
    
    * added a simple auto align
    
    * added SimpleAutoAlign to drive controller
    
    * changed SimpleAutoAlign PID temporarly to regular swerve PID
    
    * update intake speed
    
    * auto align for id 25, 8, 11, 24, 27
    
    * auto align distance update
    
    ---------
    
    Co-authored-by: Nonochen0104 <xchen26@moundsparkacademy.org>
    Co-authored-by: Lilly <113945482+raininglilly@users.noreply.github.com>

---
+++ src/main/java/frc/robot/Command/SimpleAutoAlign.java
@@ -0,0 +1,104 @@
+package frc.robot.Command;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Subsystems.SwerveSubsystem;
+
+public class SimpleAutoAlign extends Command {
+    
+    private SwerveSubsystem swerveSubsystem;
+
+    //target for how far away robot should be from hub
+    private final double targetDistance = 2.1; //in meters - temporary need to check
+
+    private final double cameraHeight = 0.5; //need to measure
+    private final double aprilTagHeight = 1; //need to measure
+    private final double cameraTilt = 0.001; //so math does not end up dividing by 0
+    
+    private final PIDController distanceController = new PIDController(0.1,0,0); //tune this
+    private final PIDController rotationController = new PIDController(0.01,0,0); //tune this
+
+    public SimpleAutoAlign(SwerveSubsystem swerveSubsystem){
+        this.swerveSubsystem = swerveSubsystem;
+        addRequirements(swerveSubsystem);
+    }
+
+    private boolean canSeeTag() {
+        double tv = NetworkTableInstance.getDefault().getTable("limelight-a").getEntry("tv").getDouble(0.0);
+        return tv > 0;
+    }
+    
+    private int getTagId() {
+        double tid = NetworkTableInstance.getDefault().getTable("limelight-a").getEntry("tid").getDouble(0.0);
+        int tidInt = (int) tid;
+        return tidInt;
+    }
+
+    private boolean isSupportedTag(int tagId) {
+        return tagId == 10 || tagId == 25 || tagId == 11 || tagId == 27 || tagId == 8 || tagId == 24;
+    }
+
+    private double getDesiredAlignmentAngle(int tagId) {
+        if (tagId == 11 || tagId == 27) {
+            return 20.0;
+        }
+        if (tagId == 8 || tagId == 24) {
+            return -20.0;
+        }
+        return 0.0;
+    }
+
+    private double getDistanceToTarget() {
+        double ty = NetworkTableInstance.getDefault().getTable("limelight-a").getEntry("ty").getDouble(0.0); // vertical angle offset in degrees
+        double angleToTargetRadians = Math.toRadians(cameraTilt + ty);
+
+        double distance = (aprilTagHeight - cameraHeight) / Math.tan(angleToTargetRadians);
+        return distance;
+    }
+
+       private double getOffsetToTarget() {
+        double xDist = NetworkTableInstance.getDefault().getTable("limelight-a").getEntry("tx").getDouble(0.0);
+        return xDist;
+    }
+
+    @Override
+    public void initialize() {
+        distanceController.reset();
+        rotationController.reset();
+    }
+
+    @Override
+    public void execute() {
+
+        double distance = getDistanceToTarget();
+        //when i say offset i mean rotation offset
+        double offset = getOffsetToTarget();
+        int tagId = getTagId();
+
+        //only auto align if distance is valid and can see tag 10
+        if (!canSeeTag() || !isSupportedTag(tagId) || distance < 0) {
+            swerveSubsystem.driveFromChassisSpeeds(new ChassisSpeeds(0,0,0), false);
+            return;
+        }
+    
+        double desiredAlignmentAngle = getDesiredAlignmentAngle(tagId);
+        double driveSpeed = distanceController.calculate(distance, targetDistance);
+        double rotationSpeed = rotationController.calculate(offset, desiredAlignmentAngle);
+
+        swerveSubsystem.driveFromChassisSpeeds(new ChassisSpeeds(driveSpeed, 0, rotationSpeed), false);
+
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        swerveSubsystem.driveFromChassisSpeeds(new ChassisSpeeds(0,0,0), false);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+
+}
